109b73969f9e8322ca778108cd0191ba30191a08
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-12-03 17:21:08 +0100

    Add paper style for posts

--- partials/singular.php
+++ partials/singular.php
@@ -1,7 +1,7 @@
 <article <?php post_class('c-article'); ?>>
 
 	<?php if (!(bool) get_field('hide_title')) : ?>
-		<header class="c-article__header">
+		<header class="c-article__header c-main__header">
 			<h1 class="c-article__title"><?php the_title(); ?></h1>
 			<time class="c-article__date" datetime="<?php echo get_the_date('c'); ?>"><?php printf(_x('Published on %s', 'sht'), get_the_date()); ?></time>
 		</header>
---
+++ single.php
@@ -0,0 +1,18 @@
+<?php
+
+get_header();
+?>
+<main class="c-main">
+	<?php
+	if (have_posts()) {
+		while (have_posts()) {
+			the_post();
+			get_template_part('partials/singular', get_post_type());
+		}
+	} else {
+		get_template_part('partials/singular', 'none');
+	}
+	?>
+</main>
+<?php
+get_footer();
