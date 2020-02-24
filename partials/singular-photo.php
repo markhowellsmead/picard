5188d41aaf4bba553ac6f9ad30b6295ea0839851
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-02-24 10:09:50 +0100

    Add photo capture date to single view

--- partials/singular-photo.php
+++ partials/singular-photo.php
@@ -3,6 +3,18 @@
 	<?php if (!(bool) get_field('hide_title')) : ?>
 		<header class="c-article__header c-main__header">
 			<h1 class="c-article__title"><?php the_title(); ?></h1>
+			<?php
+			if (has_post_thumbnail()) {
+				$thumbnail_meta = wp_get_attachment_metadata(get_post_thumbnail_id());
+				if (is_array($thumbnail_meta['image_meta']) && isset($thumbnail_meta['image_meta']['created_timestamp']) && $thumbnail_meta['image_meta']['created_timestamp'] > 0) {
+					printf(
+						'<time class="c-article__date c-article__date--captured" datetime="%1$s">%2$s</time>',
+						date_i18n('c', $thumbnail_meta['image_meta']['created_timestamp']),
+						sprintf(_x('Photographed on %s', 'date', 'picard'), date_i18n('dS F Y', $thumbnail_meta['image_meta']['created_timestamp']))
+					);
+				}
+			}
+			?>
 			<time class="c-article__date" datetime="<?php echo get_the_date('c'); ?>"><?php printf(_x('Published on %s', 'sht'), get_the_date()); ?></time>
 		</header>
 	<?php endif; ?>
