30d6afd818f36066cf1e4dbdb45c541ac384aadd
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-06-07 12:49:57 +0200

    Phortos by collection

--- src/Theme.php
+++ src/Theme.php
@@ -90,6 +90,7 @@ class Theme
 				\SayHello\Theme\Block\HomeCarousel::class,
 				\SayHello\Theme\Block\ImageGallery::class,
 				\SayHello\Theme\Block\PostHeader::class,
+				\SayHello\Theme\Block\PhotosByCollection::class,
 				\SayHello\Theme\Block\ViewpointCards::class,
 				\SayHello\Theme\Block\YearsOnline::class,
 
