815bdd9e4d412ca64174b15f8b33d45ac858c12c
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-07-25 11:18:24 +0200

    Better search results page

--- search.php
+++ search.php
@@ -1,3 +1,46 @@
-<?php
+<?php get_header(); ?>
 
-include('archive.php');
+<main class="c-main">
+	<section class="c-archive">
+
+		<header class="c-archive__header">
+			<?php
+			the_archive_title('<h1 class="c-archive__title">', '</h1>');
+			if (!empty($term_description = term_description())) {
+				printf(
+					'<div class="c-archive__description">%s</div>',
+					apply_filters('the_content', $term_description)
+				);
+			}
+
+			if (is_search()) {
+				get_search_form();
+			}
+			?>
+		</header>
+
+		<div class="c-archive__content">
+			<?php
+			if (have_posts()) {
+				while (have_posts()) {
+					the_post();
+					get_template_part('partials/excerpt');
+				}
+			}
+			?>
+		</div>
+
+		<?php
+		$paginate = paginate_links([
+			'mid_size' => 8
+		]);
+		if ('' != $paginate) {
+			echo '<div class="c-pagination">';
+			echo '<div class="c-pagination__content">' .$paginate. '</div>';
+			echo '</div>';
+		}
+		?>
+	</section>
+</main>
+
+<?php get_footer();
