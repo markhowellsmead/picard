3bf1179386402ac87456fe93abdcad787b04f4c8
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-08-02 13:49:02 +0200

    Update image gallery PHP template

--- partials/block/image-gallery.php
+++ partials/block/image-gallery.php
@@ -3,16 +3,23 @@
 use SayHello\Theme\Package\Lazysizes;
 
 if (empty($images = get_field('images'))) {
+	if ($data['is_context_edit'] ?? false) {
+		?>
+		<section class="wp-block-sht-imagegallery wp-block-sht-imagegallery--empty <?php echo !empty($data['align']) ? ' align'.$data['align'] : '';?>">
+			<p class=""><strong><?php echo $data['title'];?></strong>: <?php _ex('Keine Bilder ausgewählt', 'Editor block message', 'sha');?></p>
+		</section>
+		<?php
+	}
 	return;
 }
 
-$target_height = 500;
-$image_size = 'large';
-
-if (count($images) > 10) {
-	$target_height = 400;
-	$image_size = 'medium';
-}
+$target_height = 280;
+$image_size = 'medium';
+//
+// if (count($images) > 10) {
+// 	$target_height = 400;
+// 	$image_size = 'medium';
+// }
 
 $unique = uniqid();
 
@@ -28,15 +35,34 @@ $unique = uniqid();
 			$flex_grow = $width * 100 / $height;
 			$flex_basis = $width * $target_height / $height;
 			$padding_bottom = ($height / $width) * 100;
-			$href = $image['sizes']['block_full'] ?? $image['sizes']['gutenberg_wide'];
+			$href = $data['is_context_edit'] ? '#' : $image['sizes']['full'] ?? $image['sizes']['gutenberg_wide'];
 			?>
 		<li
 			class="wp-block-sht-imagegallery__entry c-grid500__item"
 			style="flex-grow:<?php echo $flex_grow;?>;flex-basis:<?php echo $flex_basis;?>px;">
+
+			<?php
+			if ($data['is_context_edit']) {
+				?>
+				<span class="c-grid500__itemlink">
+					<i class="c-grid500__uncollapse" style="padding-bottom:<?php echo $padding_bottom;?>%"></i>
+					<?php
+					$image = wp_get_attachment_image($image['ID'], $source_image_size, false, ['class' => 'c-grid500__image']);
+					if (!empty($image)) {
+						$image = '<figure class="c-grid500__figure">'.$image.'</figure>';
+					}
+					echo $image;
+					?>
+				</span>
+			<?php } else {?>
 				<a class="c-grid500__itemlink" href="<?php echo $href;?>" data-fancybox="gallery-<?php echo $unique;?>" data-caption="<?php echo $image['caption'] ?? $image['title'];?>">
 					<i class="c-grid500__uncollapse" style="padding-bottom:<?php echo $padding_bottom;?>%"></i>
-					<?php echo Lazysizes::getLazyImage($image['ID'], $source_image_size, 'c-grid500__figure', 'c-grid500__image');?>
+					<?php
+					echo Lazysizes::getLazyImage($image['ID'], $source_image_size, 'c-grid500__figure', 'c-grid500__image');
+					?>
 				</a>
+				<?php
+			} ?>
 			</li>
 		<?php }?>
 	</ul>
