d4724c5e6120d51b6af9e0beda05e103a4096a64
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-12-27 13:20:22 +0100

    Fix subpages HTML

--- partials/block/subpages.php
+++ partials/block/subpages.php
@@ -17,7 +17,7 @@ if (empty($args['pages'])) {
 		<?php
 
 		foreach ($args['pages'] as $page) {
-			$thumbnail = '<div class="wp-block-mhm-subpages__figure wp-block-mhm-subpages__figure--empty"><a href="' . get_permalink($page->ID) . '"></a></div>';
+			$thumbnail = '<div class="wp-block-mhm-subpages__figure wp-block-mhm-subpages__figure--empty"></div>';
 
 			if (has_post_thumbnail($page->ID)) {
 				$thumbnail = '<a href="' . get_permalink($page->ID) . '">' . Lazysizes::getLazyImage(get_post_thumbnail_id($page->ID), 'card', 'wp-block-mhm-subpages__figure', 'wp-block-mhm-subpages__image') . '</a>';
@@ -29,7 +29,7 @@ if (empty($args['pages'])) {
 				get_permalink($page->ID),
 				$thumbnail,
 				'<h3 class="wp-block-mhm-subpages__entrytitle"><a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></h3>',
-				'<time class="wp-block-mhm-subpages__entrydate">' . sprintf(_x('Last updated on %s', '', 'picard'), date(get_option('date_format'), strtotime($page->post_date))) . '</time>',
+				'<time class="wp-block-mhm-subpages__entrydate">' . sprintf(_x('Last updated on %s', '', 'picard'), date(get_option('date_format'), strtotime($page->post_modified))) . '</time>',
 				'' //!empty($album->description) ? '<div class="wp-block-mhm-subpages__entrydescription">' . $album->description . '</div>' : ''
 			);
 		}
