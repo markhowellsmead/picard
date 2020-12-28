6d2aa26c78c05c4d66a897df3c1bdfbe5027e46d
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-12-28 14:31:39 +0100

    Fix grid stuff

--- partials/block/image-gallery.php
+++ partials/block/image-gallery.php
@@ -4,11 +4,11 @@ use SayHello\Theme\Package\Lazysizes;
 
 if (empty($images = get_field('images'))) {
 	if ($args['is_context_edit'] ?? false) {
-		?>
-		<section class="wp-block-sht-imagegallery wp-block-sht-imagegallery--empty <?php echo !empty($args['align']) ? ' align'.$args['align'] : '';?>">
-			<p class=""><strong><?php echo $args['title'];?></strong>: <?php _ex('Keine Bilder ausgewählt', 'Editor block message', 'sha');?></p>
+?>
+		<section class="wp-block-sht-imagegallery wp-block-sht-imagegallery--empty <?php echo !empty($args['align']) ? ' align' . $args['align'] : ''; ?>">
+			<p class=""><strong><?php echo $args['title']; ?></strong>: <?php _ex('Keine Bilder ausgewählt', 'Editor block message', 'sha'); ?></p>
 		</section>
-		<?php
+<?php
 	}
 	return;
 }
@@ -22,7 +22,7 @@ $image_size = 'medium';
 
 $align = $args['align'];
 if (!empty($args['align'])) {
-	$align = 'align'.$args['align'];
+	$align = 'align' . $args['align'];
 }
 
 $unique = uniqid();
@@ -30,52 +30,52 @@ $unique = uniqid();
 ?>
 
 <!-- Grid layout origin: https://github.com/xieranmaya/blog/issues/6 #wowza -->
-<section class="wp-block-sht-imagegallery <?php echo $align;?>">
-	<ul class="wp-block-sht-imagegallery__images c-grid500">
-		<?php foreach ($images as $image) {
-			$source_image_size = $image['sizes'][$image_size] ?? null ? $image_size : 'large';
-			$width = $image['sizes'][$source_image_size.'-width'] ?? $image['width'];
-			$height = $image['sizes'][$source_image_size.'-height'] ?? $image['height'];
+<section class="wp-block-sht-imagegallery <?php echo $align; ?>">
+	<div class="wp-block-sht-imagegallery__images c-grid500">
+		<div class="c-grid500__inner">
+			<?php foreach ($images as $image) {
+				$source_image_size = $image['sizes'][$image_size] ?? null ? $image_size : 'large';
+				$width = $image['sizes'][$source_image_size . '-width'] ?? $image['width'];
+				$height = $image['sizes'][$source_image_size . '-height'] ?? $image['height'];
 
-			if ($width < 100 || $height < 100) {
-				$file_path = get_attached_file($image['ID']);
-				$file_dims = getimagesize($file_path);
-				$width = $file_dims[0];
-				$height = $file_dims[1];
-			}
+				if ($width < 100 || $height < 100) {
+					$file_path = get_attached_file($image['ID']);
+					$file_dims = getimagesize($file_path);
+					$width = $file_dims[0];
+					$height = $file_dims[1];
+				}
 
-			$flex_grow = $width * 100 / $height;
-			$flex_basis = $width * $target_height / $height;
-			$padding_bottom = ($height / $width) * 100;
-			$href = $args['is_context_edit'] ?? false ? '#' : $image['sizes']['full'] ?? $image['sizes']['gutenberg_wide'];
+				$flex_grow = $width * 100 / $height;
+				$flex_basis = $width * $target_height / $height;
+				$padding_bottom = ($height / $width) * 100;
+				$href = $args['is_context_edit'] ?? false ? '#' : $image['sizes']['full'] ?? $image['sizes']['gutenberg_wide'];
 			?>
-		<li
-			class="wp-block-sht-imagegallery__entry c-grid500__item"
-			style="flex-grow:<?php echo $flex_grow;?>;flex-basis:<?php echo $flex_basis;?>px;">
+				<div class="wp-block-sht-imagegallery__entry c-grid500__item" style="flex-grow:<?php echo $flex_grow; ?>;flex-basis:<?php echo $flex_basis; ?>px;">
 
-			<?php
-			if ($args['is_context_edit'] ?? false) {
-				?>
-				<span class="c-grid500__itemlink">
-					<i class="c-grid500__uncollapse" style="padding-bottom:<?php echo $padding_bottom;?>%"></i>
 					<?php
-					$image = wp_get_attachment_image($image['ID'], $source_image_size, false, ['class' => 'c-grid500__image']);
-					if (!empty($image)) {
-						$image = '<figure class="c-grid500__figure">'.$image.'</figure>';
-					}
-					echo $image;
+					if ($args['is_context_edit'] ?? false) {
 					?>
-				</span>
-			<?php } else {?>
-				<a class="c-grid500__itemlink" href="<?php echo $href;?>" data-fancybox="gallery-<?php echo $unique;?>" data-caption="<?php echo $image['caption'] ?? $image['title'];?>">
-					<i class="c-grid500__uncollapse" style="padding-bottom:<?php echo $padding_bottom;?>%"></i>
+						<span class="c-grid500__itemlink">
+							<i class="c-grid500__uncollapse" style="padding-bottom:<?php echo $padding_bottom; ?>%"></i>
+							<?php
+							$image = wp_get_attachment_image($image['ID'], $source_image_size, false, ['class' => 'c-grid500__image']);
+							if (!empty($image)) {
+								$image = '<figure class="c-grid500__figure">' . $image . '</figure>';
+							}
+							echo $image;
+							?>
+						</span>
+					<?php } else { ?>
+						<a class="c-grid500__itemlink" href="<?php echo $href; ?>" data-fancybox="gallery-<?php echo $unique; ?>" data-caption="<?php echo $image['caption'] ?? $image['title']; ?>">
+							<i class="c-grid500__uncollapse" style="padding-bottom:<?php echo $padding_bottom; ?>%"></i>
+							<?php
+							echo Lazysizes::getLazyImage($image['ID'], $source_image_size, 'c-grid500__figure', 'c-grid500__image');
+							?>
+						</a>
 					<?php
-					echo Lazysizes::getLazyImage($image['ID'], $source_image_size, 'c-grid500__figure', 'c-grid500__image');
-					?>
-				</a>
-				<?php
-			} ?>
-			</li>
-		<?php }?>
-	</ul>
+					} ?>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
 </section>
