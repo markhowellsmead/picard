f78e95a5854108feb629030bba50fabd60021e75
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-12-29 17:57:07 +0100

    DIsallow multiple ancesotr blocks

--- .build/assets/gutenberg/blocks/viewpoint-ancestors/index.js
+++ .build/assets/gutenberg/blocks/viewpoint-ancestors/index.js
@@ -4,22 +4,22 @@ import { ServerSideRender } from '@wordpress/components';
 import { Component } from '@wordpress/element';
 
 registerBlockType('mhm/viewpoint-ancestors', {
-	title: _x('Viewpoint ancestors', 'Block title', 'sha'),
-	icon: 'images',
-	category: 'widgets',
-	supports: {
-		align: ['wide', 'full'],
-		mode: false,
-		html: false,
-		multiple: true,
-		reusable: true,
-	},
-	edit: class extends Component {
-		render() {
-			return <ServerSideRender block='mhm/viewpoint-ancestors' />;
-		}
-	},
-	save() {
-		return null;
-	},
+    title: _x('Viewpoint ancestors', 'Block title', 'sha'),
+    icon: 'layout',
+    category: 'widgets',
+    supports: {
+        align: ['wide', 'full'],
+        mode: false,
+        html: false,
+        multiple: false,
+        reusable: true,
+    },
+    edit: class extends Component {
+        render() {
+            return <ServerSideRender block='mhm/viewpoint-ancestors' />;
+        }
+    },
+    save() {
+        return null;
+    },
 });
