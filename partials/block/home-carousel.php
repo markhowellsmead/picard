31ea40de6803a51ca9f3ed4cf590d163b7d36460
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-02-17 23:40:46 +0100

    Use MU methods

--- partials/block/home-carousel.php
+++ partials/block/home-carousel.php
@@ -1,20 +1,18 @@
 <?php
 
-use SayHello\Theme\Package\Lazysizes;
-
 if (empty($data ?? [])) {
 	return;
 }
 
 if (empty($data['entries'] ?? [])) {
-	if (sht_theme()->Package->Gutenberg->isContextEdit()) {
-		?>
+	if (pt_must_use_get_instance()->Package->Gutenberg->isContextEdit()) {
+?>
 		<section class="b-home-carousel b-home-carousel--noselection">
 			<div class="c-editormessage c-editormessage--noselection">
 				<p>
-					<strong><?php _x('Home page carousel', 'Editor message title', 'sha');?></strong>
+					<strong><?php _x('Home page carousel', 'Editor message title', 'sha'); ?></strong>
 				</p>
-				<p><?php _x('No entries available 😥', 'Editor message', 'sha');?></p>
+				<p><?php _x('No entries available 😥', 'Editor message', 'sha'); ?></p>
 			</div>
 		</section>
 	<?php }
@@ -28,9 +26,9 @@ if (empty($data['entries'] ?? [])) {
 
 	?>
 	<section class="c-block b-home-carousel alignfull">
-		<div class="swiper-container<?php echo $extra_class;?>">
+		<div class="swiper-container<?php echo $extra_class; ?>">
 			<div class="b-home-carousel__entries swiper-wrapper">
-				<?php foreach ($data['entries'] as $entry) {?>
+				<?php foreach ($data['entries'] as $entry) { ?>
 					<article class="b-home-carousel__entry swiper-slide">
 						<figure class="b-home-carousel__figure">
 							<div class="b-home-carousel__imagewrap">
@@ -38,22 +36,22 @@ if (empty($data['entries'] ?? [])) {
 								echo wp_get_attachment_image($entry['image']['ID'], $image_size);
 								?>
 							</div>
-							<figcaption class="b-home-carousel__caption"><?php echo $entry['legend'];?></figcaption>
+							<figcaption class="b-home-carousel__caption"><?php echo $entry['legend']; ?></figcaption>
 							<?php
-							if (!empty($entry['link']['url'])) {?>
-								<a class="b-home-carousel__link" href="<?php echo $entry['link']['url'];?>"></a>
-							<?php }?>
+							if (!empty($entry['link']['url'])) { ?>
+								<a class="b-home-carousel__link" href="<?php echo $entry['link']['url']; ?>"></a>
+							<?php } ?>
 						</figure>
 					</article>
-					<?php
-				}?>
+				<?php
+				} ?>
 			</div>
-			<?php if (count($data['entries']) > 1) {?>
+			<?php if (count($data['entries']) > 1) { ?>
 				<div class="b-home-carousel__paginationwrap">
 					<div class="b-home-carousel__pagination b-home-carousel__pagination--prev swiper-button-prev"></div>
 					<div class="b-home-carousel__pagination b-home-carousel__pagination--next swiper-button-next"></div>
 				</div>
-			<?php }?>
+			<?php } ?>
 		</div>
 	</section>
 <?php }
