86f5045627d27db086b349774d19cc2defa28719
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-05-17 00:12:05 +0200

    Add gallery ID to gallery blocks

--- .build/assets/scripts/ui/fancybox.js
+++ .build/assets/scripts/ui/fancybox.js
@@ -1,6 +1,20 @@
 require('@fancyapps/fancybox');
 
 (function ($) {
+    document.querySelectorAll('.wp-block-gallery').forEach(gallery => {
+        const links = gallery.querySelectorAll(
+            'a[href$=".jpg"]:not([target]):not([download]), a[href$=".png"]:not([target]):not([download]), a[href$=".gif"]:not([target]):not([download]), a[href$=".svg"]:not([target]):not([download]), a[href$=".webp"]:not([target]):not([download])'
+        );
+
+        if (!!links.length) {
+            const random = Math.floor(Math.random() * 10000);
+            const gallery_id = `gallery_${random}`;
+            links.forEach(link => {
+                link.setAttribute('data-fancybox', gallery_id);
+            });
+        }
+    });
+
     $(
         "a[href$='.jpg']:not([target]):not([download]), a[href$='.png']:not([target]):not([download]), a[href$='.gif']:not([target]):not([download]), a[href$='.svg']:not([target]):not([download]), a[href$='.webp']:not([target]):not([download])"
     ).fancybox({
