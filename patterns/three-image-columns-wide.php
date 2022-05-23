fb5286a85b5210d8f827f292824f5b1ceeefa4dc
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-05-23 13:45:13 +0200

    Add three image columns wide pattern

---
+++ patterns/three-image-columns-wide.php
@@ -0,0 +1,27 @@
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide">
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:image -->
+		<figure class="wp-block-image"><img alt="" /></figure>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:image -->
+		<figure class="wp-block-image"><img alt="" /></figure>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:image -->
+		<figure class="wp-block-image"><img alt="" /></figure>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
